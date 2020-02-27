4411228043699e5143630dfb4cb71d5815c657ce
Guilherme Augusto <guilhermejoazeiro@gmail.com>
2020-02-27 17:03:36 -0300

    Add mongoose

--- server.js
+++ server.js
@@ -1,11 +1,21 @@
 const express = require('express')
 const dotenv = require('dotenv')
-
-const bootcamps = require('./routes/bootcamps')
+const morgan = require('morgan')
+const connectDB = require('./config/db')
 
 dotenv.config({ path: './config/config.env' })
 
+connectDB()
+
+const bootcamps = require('./routes/bootcamps')
+
+
 const app = express();
+
+
+if (process.env.NODE_ENV === 'development') {
+    app.use(morgan('dev'))
+}
 
 app.use('/api/v1/bootcamps', bootcamps)
 
