10a0bacdb48baf9b2cf10ddd2fc5bb4ef30466a9
Guilherme Augusto <guilhermejoazeiro@gmail.com>
2020-03-02 14:46:32 -0300

    Add bootcamps pagination

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -23,22 +23,51 @@ exports.getBootcamps = asyncHandler(async (req, res, next) => {
     queryStr = queryStr.replace(/\b(gt|gte|lt|lte|in)\b/g, match => `$${match}`)
 
     query = Bootcamp.find(JSON.parse(queryStr))
-       
+
+    //Sort   
     if (req.query.select) {
         const fields = req.query.select.split(',').join(' ')
         query = query.select(fields)
     }
 
-    if(req.query.sort){
+    if (req.query.sort) {
         const sortBy = req.query.sort.split(',').join(' ')
         query = query.sort(sortBy)
-    }else{
+    } else {
         query = query.sort('-createdAt')
     }
 
+    //Pagination
+    const page = parseInt(req.query.page, 10) || 1
+    const limit = parseInt(req.query.limit, 10) || 25
+
+    const startIndex = (page - 1) * limit
+    const endIndex = page * limit
+    const total = await Bootcamp.countDocuments()
+
+    query = query.skip(startIndex).limit(limit)
+
+    //Executing query
     const bootcamps = await query
 
-    res.status(200).json({ success: true, count: bootcamps.length, data: bootcamps })
+    //Pagination result
+    const pagination = {}
+
+    if (endIndex < total) {
+        pagination.next = {
+            page: page + 1,
+            limit
+        }
+    }
+    if (startIndex > 0) {
+        pagination.prev = {
+            page: page - 1,
+            limit
+        }
+    }
+
+
+    res.status(200).json({ success: true, count: bootcamps.length, pagination, data: bootcamps })
 
 
 
