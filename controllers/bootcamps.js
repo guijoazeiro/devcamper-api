674816d5b08a53910a945484807541a582b31338
Guilherme Augusto <guilhermejoazeiro@gmail.com>
2020-03-02 12:23:44 -0300

    Add advanced filtering

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -4,8 +4,15 @@ const geocoder = require('../utils/geocoder')
 const Bootcamp = require('../models/Bootcamp')
 
 exports.getBootcamps = asyncHandler(async (req, res, next) => {
+    let query;
 
-    const bootcamps = await Bootcamp.find()
+    let queryStr = JSON.stringify(req.query)
+
+    queryStr = queryStr.replace(/\b(gt|gte|lt|lte|in)\b/g, match => `$${match}`)
+
+    query = Bootcamp.find(JSON.parse(queryStr))
+
+    const bootcamps = await query
 
     res.status(200).json({ success: true, count: bootcamps.length, data: bootcamps })
 
