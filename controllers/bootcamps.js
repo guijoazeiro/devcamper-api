bd1db536e644bd4beaa8478baed1bd39419f88c6
Guilherme Augusto <guilhermejoazeiro@gmail.com>
2020-03-02 11:41:56 -0300

    Add geospatial query

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -1,5 +1,6 @@
 const ErrorResponse = require('../utils/errorResponse')
 const asyncHandler = require('../middleware/async')
+const geocoder = require('../utils/geocoder')
 const Bootcamp = require('../models/Bootcamp')
 
 exports.getBootcamps = asyncHandler(async (req, res, next) => {
@@ -54,4 +55,26 @@ exports.deleteBootcamp = asyncHandler(async (req, res, next) => {
     }
     res.status(200).json({ success: true, data: {} })
 
+})
+
+
+//Get bootcamps within a radius
+exports.getBootcampsInRadius = asyncHandler(async (req, res, next) => {
+    const { zipcode, distance } = req.params
+
+    const loc = await geocoder.geocode(zipcode)
+    const lat = loc[0].latitude
+    const lng = loc[0].longitude
+
+    //Calc radius using radians
+    const radius = distance / 3963
+
+    const bootcamps = await Bootcamp.find({
+        location: { $geoWithin: { $centerSphere: [[lng, lat], radius] } }
+    })
+    res.status(200).json({
+        success: true,
+        count: bootcamps.length,
+        data: bootcamps
+    })
 })
