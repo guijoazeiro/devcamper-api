12b11ed5825b24c26707db8ae9443f375301e743
Guilherme Augusto <guilhermejoazeiro@gmail.com>
2020-03-06 15:42:00 -0300

    Add auth protect middleware

--- routes/auth.js
+++ routes/auth.js
@@ -2,10 +2,14 @@ const express = require('express')
 const router = express.Router()
 const {
     register,
-    login
+    login,
+    getsMe
 } = require('../controllers/auth')
 
+const {protect} = require('../middleware/auth')
+
 router.post('/register', register)
-router.post('/login', login)
+router.post('/login', login),router.get('/me', protect, getsMe)
+
 
 module.exports = router
--- routes/bootcamps.js
+++ routes/bootcamps.js
@@ -17,6 +17,8 @@ const advancedResults = require('../middleware/advancedResults')
 //Include other resource routers
 const courseRouter = require('./courses')
 
+const { protect } = require('../middleware/auth')
+
 //Re-route into other resources
 router.use('/:bootcampId/courses', courseRouter)
 
@@ -24,15 +26,15 @@ router.route('/radius/:zipcode/:distance').get(getBootcampsInRadius)
 
 router
     .route('/')
-    .get(advancedResults(Bootcamp,'courses'), getBootcamps)
-    .post(createBootcamp)
+    .get(advancedResults(Bootcamp, 'courses'), getBootcamps)
+    .post(protect, createBootcamp)
 
 router
     .route('/:id')
     .get(getBootcamp)
-    .put(updateBootcamp)
-    .delete(deleteBootcamp)
+    .put(protect, updateBootcamp)
+    .delete(protect, deleteBootcamp)
 
-router.route('/:id/photo').put(bootcampPhotoUpload)    
+router.route('/:id/photo').put(protect, bootcampPhotoUpload)
 
 module.exports = router
