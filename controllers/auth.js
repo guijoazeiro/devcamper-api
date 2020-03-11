c5b5d898521c8bc13ad5a45f30556a46984a2055
Guilherme Augusto <guilhermejoazeiro@gmail.com>
2020-03-11 15:14:13 -0300

    Add forgot password

--- controllers/auth.js
+++ controllers/auth.js
@@ -72,3 +72,20 @@ exports.getsMe = asyncHandler(async (req, res, next) => {
         data: user
     })
 })
+
+
+exports.forgotPassword = asyncHandler(async (req, res, next) => {
+    const user = await User.findOne({email: req.body.email})
+
+    if(!user){
+        return next(new ErrorResponse('There is no user with that email', 404))
+    }
+    const resetToken = user.getResetPasswordToken()
+
+    await user.save({validateBeforeSave: false})
+
+    res.status(200).json({
+        success: true,
+        data: user
+    })
+})
--- routes/auth.js
+++ routes/auth.js
@@ -3,13 +3,15 @@ const router = express.Router()
 const {
     register,
     login,
-    getsMe
+    getsMe,
+    forgotPassword
 } = require('../controllers/auth')
 
 const {protect} = require('../middleware/auth')
 
 router.post('/register', register)
 router.post('/login', login),router.get('/me', protect, getsMe)
+router.post('/login', login),router.post('/forgotpassword',  forgotPassword)
 
 
 module.exports = router
